d463b8f6caaa92cad3f489703675400f8c1f7106
liwiseap <liwiseap@163.com>
2018-06-10 23:33:27 +0800

    operation-system打印日志调整

--- operation-system/src/main/java/com/tim/controller/dc/DcController.java
+++ operation-system/src/main/java/com/tim/controller/dc/DcController.java
@@ -1,14 +1,14 @@
 package com.tim.controller.dc;
 
 import com.tim.service.dc.DcClient;
-import org.apache.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
+@Slf4j
 public class DcController {
-
-    private final Logger logger = Logger.getLogger(getClass());
 
     @Autowired
     DcClient dcClient;
