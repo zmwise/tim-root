13ec2d05a8c91029e271eb04500aa50629edee52
liwiseap <liwiseap@163.com>
2018-06-10 23:36:09 +0800

    common-dto不使用数据源

--- code-generator/src/main/java/com/tim/CodeGeneratorApplication.java
+++ code-generator/src/main/java/com/tim/CodeGeneratorApplication.java
@@ -2,8 +2,9 @@ package com.tim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CodeGeneratorApplication {
 
 	public static void main(String[] args) {
