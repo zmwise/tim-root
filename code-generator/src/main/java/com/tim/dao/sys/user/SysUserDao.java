c208beadb75c5656d371bcc911f410b9b3664b0b
liwiseap <liwiseap@163.com>
2018-06-12 22:57:10 +0800

    无用提交

--- code-generator/src/main/java/com/tim/dao/sys/user/SysUserDao.java
+++ code-generator/src/main/java/com/tim/dao/sys/user/SysUserDao.java
@@ -2,7 +2,6 @@ package com.tim.dao.sys.user;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.tim.entity.sys.user.SysUser;
-import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Repository;
  * @author lizhiming
  * @since 2018-04-19
  */
-@Repository
 public interface SysUserDao extends BaseMapper<SysUser> {
 
 }
